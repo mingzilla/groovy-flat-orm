aa75481c0ad8f552523201e0add51c450d5b9bd3
ming.huang <means789@gmail.com>
2024-06-09 18:18:56 +0100

    1

--- lib/src/main/groovy/uk/co/mingzilla/example/MyApp.groovy
+++ lib/src/main/groovy/uk/co/mingzilla/example/MyApp.groovy
@@ -19,23 +19,23 @@ class MyApp {
     static void main(String[] args) {
         OrmActor.run(RepoDb.conn, { Connection conn ->
             IdGen idGen = IdGen.create()
-            List<MyPerson> people1 = OrmRead.listAll(conn, MyPerson.class)
-            List<MyPerson> people2 = MyPerson.listByNameStartsWith(conn, 'An')
-            MyPerson person = OrmRead.getById(conn, MyPerson.class, 1)
+            List<MyPerson> people1 = OrmRead.listAll(conn, MyPerson.class) // <- Example usage
+            List<MyPerson> people2 = MyPerson.listByNameStartsWith(conn, 'An') // <-
+            MyPerson person = OrmRead.getById(conn, MyPerson.class, 1) // <-
 
-            println OrmRead.count(conn, MyPerson.class)
+            println OrmRead.count(conn, MyPerson.class) // <-
             println people1*.name.join(', ')
             println people2*.name.join(', ')
             println person?.name
 
             MyPerson p = new MyPerson(id: idGen.int, name: 'Andrew')
-            OrmErrorCollector collector = OrmWrite.save(conn, p)
+            OrmErrorCollector collector = OrmWrite.save(conn, p) // <-
 
             println p.id
-            println collector.hasErrors()
+            println collector.hasErrors() // <-
             println OrmRead.count(conn, MyPerson.class)
 
-            boolean isDeleted = OrmWrite.delete(conn, p)
+            boolean isDeleted = OrmWrite.delete(conn, p) // <-
             println isDeleted
             println OrmRead.count(conn, MyPerson.class)
         })
